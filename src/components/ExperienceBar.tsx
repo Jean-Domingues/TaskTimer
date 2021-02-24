2b17de66b168482610d8cb0c9fa0c067986a9df6
Jean Domingues <jeandomingues752@gmail.com>
2021-02-23 23:36:36 -0300

    Finalizando o primeiro dia

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -4,8 +4,9 @@ export function ExperienceBar() {
       <span>0 xp</span>
       <div>
         <div style={{ width: '50%' }}>
-          <span className="current-experience">300xp</span>
-
+          <span className="current-experience" style={{ left: '50%' }}>
+            300xp
+          </span>
         </div>
       </div>
       <span>660 xp</span>
