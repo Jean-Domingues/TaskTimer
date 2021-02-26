5cb16382709ba5353a8f1a6f850a7c2f85292909
Jean Domingues <jeandomingues752@gmail.com>
2021-02-25 21:26:35 -0300

    Migrando para CSS module

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,9 @@
-import Head from 'next/head';
 import { ExperienceBar } from '../components/ExperienceBar';
 
 function Home() {
   return (
     <div className="container">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+    
       <ExperienceBar />
     </div>
   );
