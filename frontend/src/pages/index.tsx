8b09de6d7a3630460b248246abb5cc7cc4d9e05b
Jean Domingues <jeandomingues752@gmail.com>
2021-02-27 00:23:50 -0300

    Adiciona favIcon e finaliza o componente countdown

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,18 @@
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+
+import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css'
 
 function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
     
       <ExperienceBar />
 
@@ -14,6 +20,7 @@ function Home() {
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
         </div>
