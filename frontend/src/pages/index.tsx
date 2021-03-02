34ec573848e6cb2d6858f943d350641924e7f8c7
Jean Domingues <jeandomingues752@gmail.com>
2021-03-01 23:57:06 -0300

    Provendo o CountDown

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,9 +4,11 @@ import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { ChallengeBox } from '../components/ChallengeBox';
 
-import Head from 'next/head'
+import { CountdownProvider } from '../contexts/CountdownContext';
 
-import styles from '../styles/pages/Home.module.css'
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
 
 function Home() {
   return (
@@ -14,19 +16,21 @@ function Home() {
       <Head>
         <title>Inicio | move.it</title>
       </Head>
-    
+
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   );
 }
